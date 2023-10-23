1072caf5b5c1c364198317e50e5170c56a35eefc
pndog171 <132823199+pndog171@users.noreply.github.com>
2023-10-23 21:19:05 +0700

    Xong Hóa Đơn (chưa có in)

--- GUi/FormHoaDon.cs
+++ GUi/FormHoaDon.cs
@@ -19,10 +19,11 @@ namespace GUi
     {
         private readonly Khachhang khService = new Khachhang();
         private readonly Phuongtien ptService = new Phuongtien();
-        private readonly Loaiphuongtien lptService = new Loaiphuongtien();
+        private readonly HoaDonService hdService = new HoaDonService();
         private readonly TaiKhoanService tkService = new TaiKhoanService();
         private readonly Model1 context = new Model1();
         private HoaDon model = new HoaDon();
+
         public FormHoaDon()
         {
             InitializeComponent();
@@ -87,6 +88,7 @@ namespace GUi
             model.MaKH = selectedKhachHangID;
             model.TenTK = selectedNhanVien;
             model.MaXe = selectedXeID;
+            model.TongTien = decimal.Parse(txtTongTien.Text);
         }
 
         private void cbbTenXe_SelectedIndexChanged(object sender, EventArgs e)
@@ -103,7 +105,7 @@ namespace GUi
                 //txtKhachHang.Text = ...
             }
         }
-        private void SoNT(string snt)
+        /*private void SoNT(string snt)
         {
             if (string.IsNullOrEmpty(snt) || snt==" ")
             {
@@ -117,12 +119,82 @@ namespace GUi
                 decimal result = Gia * SNthue * ((Tax + 100) / 100);
                 txtTongTien.Text = result.ToString();
             }
+        }*/
+
+        
+
+        private bool checkValue()
+        {
+            if (txtHoaDon.Text == "" || cbbKhachHang.Text == "" || cbbNhanVien.Text == "" || cbbTenXe.Text == "" || txtTongTien.Text == "")
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void clearValue()
+        {
+            txtHoaDon.Text = cbbKhachHang.Text = cbbNhanVien.Text = cbbTenXe.Text = txtGia.Text = txtSoNgayThue.Text = txtVAT.Text = txtTongTien.Text = "" ;
         }
 
         //private string defaultValue = "1";
         private void txtSoNgayThue_TextChanged(object sender, EventArgs e)
         {
-            SoNT(txtSoNgayThue.Text);
+            //SoNT(txtSoNgayThue.Text);
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (checkValue() == true)
+                {
+                    TinhTien();
+                    getValue();
+                    hdService.InsertUpdate(model);
+                    MessageBox.Show("Da update thanh cong!");
+                    clearValue();
+                }
+                else throw new Exception("vui long nhap day du thong tin!!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dateNhanXe_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime inTime = Convert.ToDateTime(dateNhanXe.Text);
+            DateTime outTime = Convert.ToDateTime(dateTraXe.Text);
+            if (outTime >= inTime)
+            {
+                txtSoNgayThue.Text = outTime.Subtract(inTime).Days.ToString();
+            }
+        }
+
+        private void dateTraXe_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime inTime = Convert.ToDateTime(dateNhanXe.Text);
+            DateTime outTime = Convert.ToDateTime(dateTraXe.Text);
+            if (outTime >= inTime)
+            {
+                txtSoNgayThue.Text = outTime.Subtract(inTime).Days.ToString();
+            }
+        }
+
+        private void btnTinhTien_Click(object sender, EventArgs e)
+        {
+            TinhTien();
+        }
+
+        private void TinhTien()
+        {
+            decimal Gia = decimal.Parse(txtGia.Text);
+            decimal SNthue = decimal.Parse(txtSoNgayThue.Text);
+            decimal Tax = decimal.Parse(txtVAT.Text);
+            decimal result = Gia * SNthue * ((Tax + 100) / 100);
+            txtTongTien.Text = result.ToString();
         }
     }
 }
